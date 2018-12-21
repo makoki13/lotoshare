75009b7d382bedab7301f02835fe41ec71dc147d
pablo <pablo.makoki@gmail.com>
2018-12-21 21:12:41 +0100

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Loto Share',
       theme: ThemeData(
         // This is the theme of your application.
         //
